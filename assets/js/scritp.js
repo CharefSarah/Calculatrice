d78d473e06b71d7098cc994b45704ae60a7b1fb6
CharefSarah <charef.sarah@gmx.fr>
2022-07-24 16:51:13 +0200

    push push

---
+++ assets/js/scritp.js
@@ -0,0 +1,31 @@
+//function that display value
+function dis(val) {
+    document.getElementById("result").value += val
+}
+
+//function that evaluates the digit and return result
+function solve() {
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+//function that clear the display
+function clr() {
+    document.getElementById("result").value = ""
+}
+
+function Del() {
+    let x = document.getElementById("result").value
+    document.getElementById("result").value = x.substring(0, x.length - 1);
+}
+
+
+function Cent() {
+
+
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result") .value = ( x / 100) * percent;
+
+}
